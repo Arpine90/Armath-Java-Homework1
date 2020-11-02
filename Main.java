cf9a806dda6b43015348de4611992c68b4620633
Gevorgyan Arpine <gevorgyanarpine@gmail.com>
2020-11-02 21:08:34 +0400

    my homework

---
+++ Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* 1
+        System.out.println("Hello");
+        System.out.println("Arpine"); */
+
+        /* 2
+        byte myByte = 2;
+        System.out.println("Byte:" + myByte);
+        short my_short =1237;
+        System.out.println("Short:" + my_short);
+        int myInt=189025;
+        System.out.println("Int:" + myInt);
+        long my_long= 29755L;
+        System.out.println("Long:" + my_long);
+        float myFloat = 29.5f;
+        System.out.println("Float:" + myFloat);
+        double my_double = 2.7;
+        System.out.println("Double:" + my_double);
+        char myChar= 'A';
+        System.out.println("Char:" + myChar);
+        boolean myBoolean= true;
+        System.out.println("Boolean:" + myBoolean);  */
+
+        /* 3
+
+        int num1 = 20;
+        int num2 = 5;
+        System.out.println( num1+num2);
+        System.out.println( num1-num2);
+        System.out.println( num1 * num2);
+        System.out.println( num1 / num2);
+        System.out.println( num1 % num2);   */
+
+
+        /*4
+        int N=123;
+        int a = N %10;
+        int b= (N%100- a)/10;
+        System.out.println(b);  */
+
+
+        /* 5
+        int x= 450;
+        int t= 600;
+        System.out.println(5*x+ 4*t);  */
+
+        /* 6
+
+        int santiMetr= 7;
+        System.out.println((float) santiMetr/100 ); */
+
+        /* 7
+        int n = 1234;
+        int a= n%10;
+        int b= (n%100 -a)/10;
+        int c= n/1000;
+        int d= n/100-10*c;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(a+b+c+d);  */
+
+        /* 8
+
+        int result = 2;
+         result += 1;
+        System.out.println(result);
+
+        result -= 1;
+        System.out.println(result);
+        result += 2;
+        System.out.println(result);
+        result /= 2;
+        System.out.println(result);
+        result +=8;
+        System.out.println(result);
+        result %=7;
+        System.out.println(result);   */
+
+
+
+
+
+    }
+}
